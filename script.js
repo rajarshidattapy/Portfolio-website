e507a1ac1548bceb3998997184a9d9c167c84468
rajarshidattapy <rayan05rio@gmail.com>
2024-09-21 14:19:34 +0530

    Your commit message

--- script.js
+++ script.js
@@ -72,12 +72,6 @@ $(document).ready(function(){
     });
 });
 
-function toggleMenu(){
-    const menu=document.querySelector(".menu-links");
-    const icon=document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-    }
-    
+
     
     
